2360b6e0498d90b6cb206e0e641a9991c5907f9c
Gursharan Singh Hayer <gursharansingh4312@gmail.com>
2025-11-14 21:21:30 -0800

    remove non essential packages

--- hammerspoon/.hammerspoon/init.lua
+++
@@ -1,6 +0,0 @@
--- Filename: ~/github/dotfiles-latest/hammerspoon/init.lua
--- ~/github/dotfiles-latest/hammerspoon/init.lua
-
--- require("cursor_escape")
--- require("lgtv_init")
-require("move_mouse_to_corner")
--- scripts/.local/bin/macos/mac/obs/scene-switcher/py/readme.md
+++
@@ -1,3 +0,0 @@
-# How to run this
-
-- All the details are in my blogpost
